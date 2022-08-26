d49ab9b64c1310f235578658cef922783ed09463
BrunoDevF <brunohenriquex00@gmail.com>
2022-08-25 23:54:38 -0300

    feat: rota de categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,17 @@
 import { Request, Response, Router } from "express";
 const categoriesRouter = Router();
+
 // repositories
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 // services
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
-// instancia
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (request: Request, response: Response) => {
   createCategoryController.handle(request, response);
 });
 
 categoriesRouter.get("/", (request: Request, response: Response) => {
-  const result = categoriesRepository.list();
-  return response.status(200).json(result);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRouter };
