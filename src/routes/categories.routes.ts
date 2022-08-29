9cd870a43bd477e10e71350edbb65e1e0fa37e95
BrunoDevF <brunohenriquex00@gmail.com>
2022-08-29 20:19:50 -0300

    feat: rotas de categories e specification

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,15 @@
 import { Request, Response, Router } from "express";
 const categoriesRouter = Router();
+import multer from 'multer';
+var upload = multer({
+  dest: "./tmp",
+})
 
 // repositories
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 // services
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
 
 categoriesRouter.post("/", (request: Request, response: Response) => {
   createCategoryController.handle(request, response);
@@ -12,6 +17,10 @@ categoriesRouter.post("/", (request: Request, response: Response) => {
 
 categoriesRouter.get("/", (request: Request, response: Response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRouter.post("/import", upload.single("file"), (request: Request, response: Response) => {
+  importCategoryController.handle(request, response);
 });
 
 export { categoriesRouter };
--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,22 +1,10 @@
 import { Request, Response, Router } from "express";
 const specificationRouter = Router();
 
-// repository
-import { SpecificationRepositories } from "../modules/cars/repositories/SpecificationRepository";
-// services
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
-// instancia
-const specificationRepositories = new SpecificationRepositories();
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification'
 
 specificationRouter.post("/", (request: Request, response: Response) => {
-  try {
-    const createSpecificationService = new CreateSpecificationService(specificationRepositories);
-
-    createSpecificationService.execute(request.body);
-    return response.status(201).send();
-  } catch (error) {
-    return response.status(400).json(error);
-  }
+  createSpecificationController.handle(request, response);
 });
 
 export { specificationRouter };
