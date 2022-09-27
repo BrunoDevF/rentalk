11324e10b225033fe00823c988ae3ef55060b286
BrunoDevF <brunohenriquex00@gmail.com>
2022-09-27 09:53:31 -0300

    feat: utilizando tsyringe para realizar injecao de dependencia

--- src/modules/cars/useCases/createCategory/createCategoryController.ts
+++ src/modules/cars/useCases/createCategory/createCategoryController.ts
@@ -7,9 +7,9 @@ class CreateCategoryController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
-    const result = await container.resolve(CreateCategoryUseCase).execute({ name, description });
+    const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
     
-    // const result = await createCategoryUseCase.execute({ name, description });
+    const result = await createCategoryUseCase.execute({ name, description });
 
     return response.status(201).json(result);
   }
