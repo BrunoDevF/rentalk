8f7cc7ee1c350ff194b330a22b24efa744b114c5
adriellyEster <adriellyester9@gmail.com>
2022-10-04 20:03:36 -0300

    feat: container tsyringe

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,16 @@
-import { container } from 'tsyringe'
+import { container } from "tsyringe";
 
-import { ICategoryRepository } from '../../modules/cars/repositories/interfaces/ICategoriesRepository'
-import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository'
+import { ICategoryRepository } from "../../modules/cars/repositories/interfaces/ICategoriesRepository";
+import { ISpecificationRepository } from "../../modules/cars/repositories/interfaces/ISpecificationRepository";
+import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationRepositories } from '../../modules/cars/repositories/implementations/SpecificationRepository'
 
 container.registerSingleton<ICategoryRepository>(
-    "CategoriesRepository",
-    CategoriesRepository
+  "CategoriesRepository",
+  CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  "SpecificationRepository",
+  SpecificationRepositories
 );
