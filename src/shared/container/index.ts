f4a2e45962bc2c8acb2f3a273bd55cb426d9c839
bruno_henr <brunohenriquex00@gmail.com>
2023-01-10 20:16:20 -0300

    feat: containers tsyringe

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,14 +2,16 @@ import { container } from "tsyringe";
 
 import { ICategoryRepository } from "../../modules/cars/repositories/interfaces/ICategoriesRepository";
 import { ISpecificationRepository } from "../../modules/cars/repositories/interfaces/ISpecificationRepository";
-import { CategoriesRepository } from "../../modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { CategoriesRepositoryInMemory } from "../../modules/cars/repositories/in-memory/CategoriesRepositoryInMemory";
 import { SpecificationRepositories } from '../../modules/cars/infra/typeorm/repositories/SpecificationRepository'
 import { IUserRepository } from '../../modules/accounts/repositories/interfaces/IUserRepository'
 import { UserRepository } from '../../modules/accounts/infra/typeorm/repositories/UserRepository'
+import { ICarsRepository } from '../../modules/cars/repositories/interfaces/ICarsRepository';
+import { CarsRepositoryInMemory } from '../../modules/cars/repositories/in-memory/CarsRepositoryInMemory';
 
 container.registerSingleton<ICategoryRepository>(
-  "CategoriesRepository",
-  CategoriesRepository
+  "CategoriesRepositoryInMemory",
+  CategoriesRepositoryInMemory
 );
 
 container.registerSingleton<ISpecificationRepository>(
@@ -20,4 +22,9 @@ container.registerSingleton<ISpecificationRepository>(
 container.registerSingleton<IUserRepository>(
   "UserRepository",
   UserRepository
+)
+
+container.registerSingleton<ICarsRepository>(
+  "CarsRepository",
+  CarsRepositoryInMemory
 )
