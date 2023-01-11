1ca7f1b89df0ab7cfa9b49854d16d61ed73bf94c
bruno_henr <brunohenriquex00@gmail.com>
2023-01-11 06:45:31 -0300

    error

--- src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/updateUserAvatarController.ts
@@ -2,13 +2,16 @@ import { Request, Response } from "express";
 import { AppError } from "../../../../shared/errors/AppError";
 import { container } from "tsyringe";
 import { CreateUserAvatarUseCase } from "./updateUserAvatarUseCase";
-
-
+interface User {
+  user: any
+  file: any
+}
+type R = { Request: Request } & User
 
 class CreateUserAvatarController {
-  async handle(request: Request, response: Response): Promise<Response> {
+  async handle(request: R, response: Response): Promise<Response> {
     const avatar_file = request.file.filename
-    const { id } = request.user;
+    const { id } = request?.user;
 
     const updateUserAvatarController = container.resolve(CreateUserAvatarUseCase);
     
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,32 +1,44 @@
 import { Request, Response, NextFunction } from "express";
-import { verify } from 'jsonwebtoken'
+import { verify } from "jsonwebtoken";
 import { UserRepository } from "../../../../modules/accounts/infra/typeorm/repositories/UserRepository";
 import { AppError } from "../../../../shared/errors/AppError";
 
 interface IPayload {
-    sub: string,
-
+  sub: string;
 }
-export async function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers["authorization"];
-    if (!authHeader) {
-        throw new AppError("Token not provided", 401);
+interface User {
+  user: any;
+  file: any;
+  headers: any
+}
+type R = { Request: Request } & User;
+export async function ensureAuthenticated(
+  req: any,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) {
+    throw new AppError("Token not provided", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "a2e63ee01401aaeca78be023dfbb8c59"
+    ) as IPayload;
+    const usersRepository = new UserRepository();
+    const user = await usersRepository.findById(user_id);
+    if (!user) {
+      throw new AppError("User doesn't exist", 401);
     }
 
-    const [, token] = authHeader.split(" ");
-    try {
-        const { sub: user_id } = verify(token, "a2e63ee01401aaeca78be023dfbb8c59") as IPayload;
-        const usersRepository = new UserRepository();
-        const user = await usersRepository.findById(user_id);
-        if(!user) {
-            throw new AppError("User doesn't exist", 401);
-        }
-
-        req.user = {
-            id: user_id,
-        }
-        next();
-    } catch (error) {
-        throw new AppError("Invalid Token", 401);
-    }
+    req.user = {
+      id: user_id,
+    };
+    next();
+  } catch (error) {
+    throw new AppError("Invalid Token", 401);
+  }
 }
