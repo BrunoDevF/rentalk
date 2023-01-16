c0ddbb02927bdae0d611a565bf0ac1cf0618ee88
bruno_henr <brunohenriquex00@gmail.com>
2023-01-16 19:53:36 -0300

    refac: alterando entidade de cars

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,57 +1,74 @@
-import { v4 as uuidV4} from 'uuid';
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm"
-import { Category } from './Category';
+import { v4 as uuidV4 } from "uuid";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { Category } from "./Category";
+import { Specification } from "./Specification";
 
 @Entity("cars")
 class Car {
-    @PrimaryColumn()
-    id?: string;
+  @PrimaryColumn()
+  id?: string;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    description: string;
+  @Column()
+  description: string;
 
-    @Column()
-    daily_rate: number;
+  @Column()
+  daily_rate: number;
 
-    @Column()
-    available?: boolean;
+  @Column()
+  available?: boolean;
 
-    @Column()
-    license_plate: string;
+  @Column()
+  license_plate: string;
 
-    @Column()
-    fine_amount: number;
+  @Column()
+  fine_amount: number;
 
-    @Column()
-    brand: string;
+  @Column()
+  brand: string;
 
-    @ManyToOne(() => Category)
-    @JoinColumn({ name: "category_id" })
-    category: Category
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: "category_id" })
+  category: Category;
 
-    @Column()
-    category_id: string;
+  @ManyToMany(() => Specification)
+  @JoinTable({
+    name: "specifications_cars",
+    joinColumns: [{ name: "car_id" }],
+    inverseJoinColumns: [{ name: "specification_id" }],
+  })
+  specifications: Specification[];
 
-    @CreateDateColumn()
-    created_at?: Date
+  @Column()
+  category_id: string;
 
-    constructor(props: Omit<Car, 'created_at' | 'category'>) {
-        Object.assign(this, {
-            ...props,
-            created_at: new Date()
-        });
-        if(!this.id) {
-            this.id = uuidV4();
-            this.available = true;
-            this.created_at = new Date();
-        } else {
-            this.id = props.id;
-        }
-        
+  @CreateDateColumn()
+  created_at?: Date;
+
+  constructor(props: Omit<Car, "created_at" | "category">) {
+    Object.assign(this, {
+      ...props,
+      created_at: new Date(),
+    });
+    if (!this.id) {
+      this.id = uuidV4();
+      this.available = true;
+      this.created_at = new Date();
+    } else {
+      this.id = props.id;
     }
+  }
 }
 
-export { Car }
+export { Car };
