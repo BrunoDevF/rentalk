1f1b2272f9d04f4b38b49495f4a7aa53437fbd6f
bruno_henr <brunohenriquex00@gmail.com>
2023-01-16 19:53:23 -0300

    feat: rodas e middlewares

--- src/shared/infra/http/middlewares/ensureAdmin.ts
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response, Request } from "express";
+import { UserRepository } from "../../../../modules/accounts/infra/typeorm/repositories/UserRepository";
 
-import { UserRepository } from "src/modules/accounts/infra/typeorm/repositories/UserRepository";
-import { AppError } from "src/shared/errors/AppError";
+import { AppError } from "../../../errors/AppError";
 
 export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
     const { id } = request.user;
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 import { createCarController } from "../../../../modules/cars/useCases/createCar";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-
+import { listAvailableCarsController } from '../../../../modules/cars/useCases/listAvailableCars'
 const carsRouter = Router();
 
 carsRouter.post(
@@ -13,5 +13,8 @@ carsRouter.post(
   (request: Request, response: Response) =>
     createCarController.handle(request, response)
 );
+
+carsRouter.get("/available", (request: Request, response: Response) =>
+listAvailableCarsController.handle(request, response))
 
 export { carsRouter };
