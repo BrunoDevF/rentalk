eb3586445464c6a2d43f4706e4475fbdbeb38fc8
bruno_henr <brunohenriquex00@gmail.com>
2023-01-16 21:12:01 -0300

    feat: rota para adicionar especificacao

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,7 +3,8 @@ import { Request, Response } from "express";
 import { createCarController } from "../../../../modules/cars/useCases/createCar";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { listAvailableCarsController } from '../../../../modules/cars/useCases/listAvailableCars'
+import { listAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars";
+import { createCarSpecificationController } from "../../../../modules/cars/useCases/createCarSpecification";
 const carsRouter = Router();
 
 carsRouter.post(
@@ -13,8 +14,15 @@ carsRouter.post(
   (request: Request, response: Response) =>
     createCarController.handle(request, response)
 );
-
+carsRouter.post(
+  "/specifications/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  (request: Request, response: Response) =>
+    createCarSpecificationController.handle(request, response)
+);
 carsRouter.get("/available", (request: Request, response: Response) =>
-listAvailableCarsController.handle(request, response))
+  listAvailableCarsController.handle(request, response)
+);
 
 export { carsRouter };
